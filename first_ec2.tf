722946bef1cfb5de18ce2b36aad0f2430a9d4e0b
Stephen Walsh <Stephen.Walsh@perspectum.com>
2024-11-22 12:05:43 +0000

    3.22 Providers

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,12 +1,50 @@
+/*
+It's best practice to include the block below, specifying a provider version
+to use, ensuring compatibility and stability.
+*/
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+/*
+Then you can configure your provider(s).
+A provider block with NO ALIAS creates a DEFAULT provider configuration.
+*/
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "myec2" {
+/*
+A provider block with an ALIAS allows you to use MULTIPLE provider
+configurations from the SAME provider.
+*/
+provider "aws" {
+  alias = "canadian-aws"
+  region = "ca-central-1"
+}
+
+resource "aws_instance" "my-ec2-instance" {
   ami           = "ami-012967cc5a8c9f891"
   instance_type = "t3.micro"
   
   tags = {
     Name = "my-first-ec2"
+  }
+}
+
+resource "aws_instance" "canadian-ec2-instance" {
+  # Use the Canadian AWS provider
+  provider = aws.canadian-aws
+  
+  ami = "ami-0bcda2433f3dabc41"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "my-canadian-ec2"
   }
 }
