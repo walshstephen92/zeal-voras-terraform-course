477459d2b9697cab9acd26ef7d741f232b73dfe6
Stephen Walsh <Stephen.Walsh@perspectum.com>
2025-03-11 17:23:28 +0000

    "Dealing with Documentation Code Updates" lesson + terraform fmt

--- firewall.tf
+++ firewall.tf
@@ -1,18 +1,41 @@
-resource "aws_security_group" "allow_tls" {
-    name = "terraform-firewall"
-    description = "Managed from Terraform"
+# New approach: separates the rules into different resources.
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "Managed from Terraform"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-    ip_protocol = "tcp"
-    security_group_id = aws_security_group.allow_tls.id
-    from_port = 80
-    to_port = 80
-    cidr_ipv4 = "0.0.0.0/0"
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_from_all_ipv4" {
+  security_group_id = aws_security_group.my_security_group.id
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-    security_group_id = aws_security_group.allow_tls.id
-    ip_protocol = "-1" # equivalent to "all ports / protocols"
-    cidr_ipv4 = "0.0.0.0/0"
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_to_all_ipv4" {
+  security_group_id = aws_security_group.my_security_group.id
+  ip_protocol       = "-1" # equivalent to "all ports / protocols"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+
+
+# Old approach: all rules in the same resource.
+resource "aws_security_group" "old_security_group" {
+  name        = "old-security-group"
+  description = "Managed from Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- first_ec2.tf
+++ first_ec2.tf
@@ -6,11 +6,11 @@ For non-Hashicorp providers, it's not just a best practice, it's required.
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
   }
@@ -29,7 +29,7 @@ A provider block with an ALIAS allows you to use MULTIPLE provider
 configurations from the SAME provider.
 */
 provider "aws" {
-  alias = "canadian-aws"
+  alias  = "canadian-aws"
   region = "ca-central-1"
 }
 
@@ -42,7 +42,7 @@ provider "azurerm" {}
 resource "aws_instance" "my-ec2-instance" {
   ami           = "ami-012967cc5a8c9f891"
   instance_type = "t3.micro"
-  
+
   tags = {
     Name = "my-first-ec2"
   }
@@ -51,8 +51,8 @@ resource "aws_instance" "my-ec2-instance" {
 resource "aws_instance" "my-canadian-ec2-instance" {
   # Use the Canadian AWS provider
   provider = aws.canadian-aws
-  
-  ami = "ami-0bcda2433f3dabc41"
+
+  ami           = "ami-0bcda2433f3dabc41"
   instance_type = "t3.micro"
 
   tags = {
